5344d877ca6afc5b3af97969135b40acd8fb8619
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2025-04-16 10:04:17 +0530

    remove bg color

--- src/components/TriangleLogic.tsx
+++ src/components/TriangleLogic.tsx
@@ -33,7 +33,7 @@ const TriangleLogic = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center px-4">
-      <div className="bg-white p-6 rounded shadow-md w-full max-w-xl">
+      <div className="p-6 rounded shadow-md w-full max-w-xl">
         <h1 className="text-2xl font-bold mb-6 text-center">
         Triangle Validator
         </h1>
